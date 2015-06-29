9443c3a0c2b5df8ac251c9a7ae072a2d0044c7cf
alcjamgit <alcarazjames@gmail.com>
2015-06-29 19:39:11 +0800

    Updating styles

---
+++ Classified.Web/Areas/Advertisement/Controllers/ListingController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Classified.Web.Areas.Advertisement.Controllers
+{
+    public class ListingController : Controller
+    {
+        // GET: Advertisement/Advertisement
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
